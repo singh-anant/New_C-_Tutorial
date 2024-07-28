7096cdb37646f1ba430a8a08824dce2c6d4245ad
anantsingh <anant.singh@mismosystems.com>
2024-07-28 15:58:30 +0530

    Work on WriteLine() and ReadLIne() and create new project.

---
+++ CSharp_Tutorial_New/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What is you age??");
+string age=Console.ReadLine();
+//here + is also used to to do concatination.
+Console.WriteLine("You entereted age-:"+age);
+//another way...
+Console.WriteLine("You are {0} years old",age);
+
+//another way 1...
+Console.WriteLine($"You are {age} years old");
+
